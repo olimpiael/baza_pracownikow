bd76f22196f05e7a658d3a336c0b8a60f8f160f1
olimpiael <148218340+olimpiael@users.noreply.github.com>
2025-07-30 12:00:11 +0200

    Update wsgi.py

--- baza_pracownikow/wsgi.py
+++ baza_pracownikow/wsgi.py
@@ -12,4 +12,14 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baza_pracownikow.settings')
 
+# --- DODAJEMY KOD DO TWORZENIA SUPERUSERA ---
+import django
+django.setup()
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'test')
+# ---------------------------------------------
+
 application = get_wsgi_application()
