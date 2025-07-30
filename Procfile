d1880e491406afb364fde72aeacf835474aa7368
olimpiael <olisiael@gmail.com>
2025-07-30 13:10:12 +0200

    Add DJANGO_SETTINGS_MODULE to Procfile and reorder imports in asgi.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne -b 0.0.0.0 -p $PORT baza_pracownikow.asgi:application
+web: DJANGO_SETTINGS_MODULE=baza_pracownikow.settings daphne -b 0.0.0.0 -p $PORT baza_pracownikow.asgi:application
--- baza_pracownikow/asgi.py
+++ baza_pracownikow/asgi.py
@@ -9,13 +9,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from pracownicy import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baza_pracownikow.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from pracownicy import routing
 
 
 application = ProtocolTypeRouter({
